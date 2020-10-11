31bfc507cb8bb429d9ac99e716463b9074bcbfdb
唐煇 <tang_edgar@163.com>
2020-10-11 18:36:36 +0800

    增删改查

--- mybatis_01/src/main/java/com/tanghui/dao/UserDao.java
+++ mybatis_01/src/main/java/com/tanghui/dao/UserDao.java
@@ -14,4 +14,36 @@ public interface UserDao {
      * @return List<User>
      */
     List<User> getUserList();
+
+    /**
+     * 根据ID查询数据
+     *
+     * @param id
+     * @return User
+     */
+    User getUserById(int id);
+
+    /**
+     * 插入数据
+     *
+     * @param user
+     * @return User
+     */
+    int addUser(User user);
+
+    /**
+     * 修改数据
+     *
+     * @param user
+     * @return int
+     */
+    int updateUser(User user);
+
+    /**
+     * 根据ID删除数据
+     *
+     * @param id
+     * @return int
+     */
+    int deleteUser(int id);
 }
