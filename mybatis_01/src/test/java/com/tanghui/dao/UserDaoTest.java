4067f05abd4b680712ae4776360be1f1acd39edd
唐煇 <tang_edgar@163.com>
2020-10-11 18:36:36 +0800

    增删改查

--- mybatis_01/src/test/java/com/tanghui/dao/UserDaoTest.java
+++ mybatis_01/src/test/java/com/tanghui/dao/UserDaoTest.java
@@ -13,8 +13,9 @@ import java.util.List;
  * @Date 2020-10-11 16:08
  */
 public class UserDaoTest {
+    // 查询所有用户
     @Test
-    public void test() {
+    public void getUserList() {
         //获取SqlSession
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
@@ -24,6 +25,74 @@ public class UserDaoTest {
         for (User user : users) {
             System.out.println(user);
         }
+        // 关闭SqlSession
+        sqlSession.close();
+    }
+
+    // 根据id查询用户
+    @Test
+    public void getUserById() {
+        //获取SqlSession
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        // 执行Sql
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        User user = userDao.getUserById(1);
+        System.out.println(user);
+        // 关闭SqlSession
+        sqlSession.close();
+    }
+
+    // 添加用户
+    @Test
+    public void addUser() {
+        //获取SqlSession
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        // 执行Sql
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+
+        int num = userDao.addUser(new User(4, "赵六", "1233459"));
+        if (num > 0) {
+            System.out.println("插入成功");
+        }
+        // 提交事务
+        sqlSession.commit();
+        // 关闭SqlSession
+        sqlSession.close();
+    }
+
+    // 修改用户
+    @Test
+    public void updateUser() {
+        //获取SqlSession
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        // 执行Sql
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+
+        int num = userDao.updateUser(new User(4, "赵六", "123459"));
+        if (num > 0) {
+            System.out.println("修改成功");
+        }
+        // 提交事务
+        sqlSession.commit();
+        // 关闭SqlSession
+        sqlSession.close();
+    }
+
+    // 删除用户
+    @Test
+    public void deleteUser() {
+        //获取SqlSession
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        // 执行Sql
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+
+        int num = userDao.deleteUser(4);
+        if (num > 0) {
+            System.out.println("删除成功");
+        }
+        // 提交事务
+        sqlSession.commit();
         // 关闭SqlSession
         sqlSession.close();
     }
