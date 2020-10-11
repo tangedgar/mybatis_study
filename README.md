46a93042b372851fa351e0e05f4c4d32251088c0
唐煇 <tang_edgar@163.com>
2020-10-11 16:18:11 +0800

    第一个mybatis程序

--- README.md
+++ README.md
@@ -3,6 +3,24 @@
 ## 数据库准备
 
 ```sql
+-- 查看数据库
+SHOW DATABASES;
 
+-- 创建数据库
+CREATE DATABASE mybatis_study
+
+-- 创建数据表
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `pwd` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+-- 插入数据
+insert into user(id, name, pwd)
+values (1, '张三', '123456'),
+       (2, '李四', '123457'),
+       (3, '王五', '123458');
 ```
 
---
+++ mybatis_01/src/main/java/com/tanghui/model/User.java
@@ -0,0 +1,45 @@
+package com.tanghui.model;
+
+/**
+ * @author 唐煇
+ * @Description
+ * @Date 2020-10-11 15:56
+ */
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
